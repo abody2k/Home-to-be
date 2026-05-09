1a6506c498394b4a5d608a3ce31fa71b53ff0453
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-05-09 14:49:00 +0300

    Implemented MC rotation

--- scripts/mc.gd
+++ scripts/mc.gd
@@ -1,1 +1,15 @@
 extends CharacterBody3D
+
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _input(event):
+	
+	if event is InputEventMouseMotion:
+		var ev = event as InputEventMouseMotion
+		
+		$arm/Camera3D.rotate_x(ev.relative.y * -0.007)
+		$arm/Camera3D.rotation_degrees.x = clampf($arm/Camera3D.rotation_degrees.x,-20,20)
+		$arm.rotate_y(ev.relative.x * -0.01)
+		$arm.rotation_degrees.y = clampf($arm.rotation_degrees.y,-89,89)
