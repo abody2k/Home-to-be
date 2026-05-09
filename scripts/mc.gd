2532bde2f733571f79c30999cd46b753653c600b
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-05-09 15:11:52 +0300

    Implemented movement system

--- scripts/mc.gd
+++ scripts/mc.gd
@@ -18,8 +18,12 @@ func _input(event):
 		$arm.rotate_x(ev.relative.y * -0.01)
 		$arm.rotation_degrees.x = clampf($arm.rotation_degrees.x,-33,33)
 		
-	var up_down = -Input.get_axis("backward","forward")
-	var left_right = Input.get_axis("left","right")
+
+var up_down = 0
+var left_right = 0
+func _physics_process(delta):
+	up_down = -Input.get_axis("backward","forward")
+	left_right = Input.get_axis("left","right")
 	if up_down == 0 and left_right ==0 :
 		velocity = Vector3.ZERO
 	velocity = transform.basis.z * up_down
