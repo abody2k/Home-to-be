3001a666a01155abab50e9ec5383ed359d395b2d
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-05-13 08:33:11 +0300

    Monster switches to hunting mode when a character is detected

--- scripts/monster.gd
+++ scripts/monster.gd
@@ -1,7 +1,13 @@
 extends CharacterBody3D
-enum MODES {WALKING, HUNTING, STANDING}
+enum MODES {WALKING, HUNTING, STANDING, GOING_HOME}
 
 
 var target : CharacterBody3D
 
 var mode : MODES = MODES.WALKING
+
+
+func _on_detector_body_entered(body):
+	if mode != MODES.GOING_HOME:
+		target = body
+		mode = MODES.HUNTING
