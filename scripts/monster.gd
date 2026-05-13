884b83a0b1011ee3a8cd4e4d01fadd4beb3c3244
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-05-13 08:42:00 +0300

    Added monster walking and standing phases

--- scripts/monster.gd
+++ scripts/monster.gd
@@ -6,6 +6,18 @@ var target : CharacterBody3D
 
 var mode : MODES = MODES.WALKING
 
+@export var path : PathFollow3D
+
+
+	
+func _physics_process(delta):
+	
+	
+	match mode:
+		MODES.STANDING:
+			return
+		MODES.WALKING:
+			path.progress_ratio+=delta
 
 func _on_detector_body_entered(body):
 	if mode != MODES.GOING_HOME:
