bc842bc44d1ea4d146b16a231014a40ba219c8e6
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-05-14 17:43:52 +0300

    Modified dialog to state that the first mission is over

--- scripts/dialog.gd
+++ scripts/dialog.gd
@@ -16,6 +16,7 @@ enum DIALOG_TYPE {ONE_TIME,REPEATING}
 var current_index=-1
 
 signal finished_dialog
+var first_mission = true
 
 var typing = false
 
@@ -33,10 +34,14 @@ func next():
 	if current_index >= dialogs.size():
 		if dialog_type == DIALOG_TYPE.REPEATING:
 			current_index = -1
+
 		else:
-			
+			if first_mission:
+				first_mission = false
+				GlobalData.mission_completed()			
 			finished_dialog.emit()
 			visible = false
+			
 		return
 	var tween = create_tween()
 	tween.finished.connect(func(): typing = false)
