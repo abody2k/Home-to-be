f4401c318896e4d300537c524b8c57ebdecdf90e
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-05-15 10:26:17 +0300

    Finished the boat mission

--- scripts/boat.gd
+++ scripts/boat.gd
@@ -4,7 +4,10 @@ extends CharacterBody3D
 @export var path : PathFollow3D
 
 
-func _on_area_3d_body_entered(body):
+func finish_job():
 	GlobalData.mission_completed()
 	GlobalData.start_new_mission()
-	create_tween().tween_property(path,"progress_ratio",1.0,30)
+		
+func _on_area_3d_body_entered(body):
+	finish_job()
+	create_tween().tween_property(path,"progress_ratio",1.0,30).finished.connect(finish_job)
