6e884b7f55e3e1dc1b9be3add4a60f564d81bbc3
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-05-15 10:09:44 +0300

    Started boat mission

--- scripts/boat.gd
+++ scripts/boat.gd
@@ -1,1 +1,10 @@
 extends CharacterBody3D
+
+
+@export var path : PathFollow3D
+
+
+func _on_area_3d_body_entered(body):
+	GlobalData.mission_completed()
+	GlobalData.start_new_mission()
+	create_tween().tween_property(path,"progress_ratio",1.0,30)
