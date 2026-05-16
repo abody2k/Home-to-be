9d3b917a10c9f17635e0961783720d63c7612df5
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-05-16 16:04:24 +0300

    Updated mc and fixed animation, also imported new models

--- scripts/mc.gd
+++ scripts/mc.gd
@@ -2,7 +2,7 @@ extends CharacterBody3D
 
 
 @export var destination : Node3D
-const SPEED = 10.0
+const SPEED = 20.0
 const AIMING_ARM_OFFSET = -0.791
 
 const NORMAL_ARM_OFFSET = 4.0
@@ -192,7 +192,7 @@ func _physics_process(delta):
 		return
 	if up_down == 0 and left_right ==0 :
 		velocity = Vector3.DOWN * 4
-		$AnimationPlayer.play("rig_005|mc_idle")
+		$AnimationPlayer.play("mc|mc_idle")
 		move_and_slide()
 		return
 		
@@ -200,7 +200,7 @@ func _physics_process(delta):
 	velocity += transform.basis.x * left_right
 	
 	velocity *= SPEED
-	$AnimationPlayer.play("rig_005|mc_walking_001")
+	$AnimationPlayer.play("mc|mc_walking")
 	move_and_slide()
 	
 
