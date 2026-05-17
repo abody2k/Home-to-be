cb569e10656bc9e6046b01a8f6c85db6b47a77b8
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-05-17 21:38:32 +0300

    Added boat to mc script

--- scripts/mc.gd
+++ scripts/mc.gd
@@ -4,7 +4,7 @@ extends CharacterBody3D
 @export var destination : Node3D
 const SPEED = 10.0
 const AIMING_ARM_OFFSET = -0.791
-
+@export var boat : CharacterBody3D
 const NORMAL_ARM_OFFSET = 4.0
 
 enum MODES {
