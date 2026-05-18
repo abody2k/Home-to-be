6a667e28d638da6bcf13f1c74b2c614dbbe48a49
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-05-18 08:22:08 +0300

    Fixed solider no rotation error

--- scenes/solider.gd
+++ scenes/solider.gd
@@ -8,7 +8,10 @@ func _physics_process(delta):
 		var dir = player.global_position - global_position
 		dir.y = 0
 		var normalized_dir = dir.normalized()
-		look_at(normalized_dir)
-		velocity = normalized_dir
-		move_and_slide()
+		var target = player.global_position
+		target.y =global_position.y
+		look_at(target)
+		velocity = normalized_dir * 15
+		if global_position.distance_to(player.global_position) > 15:
+			move_and_slide()
 		
