2480e6962fa128dedda749b23f2efa697b81346c
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-05-18 08:15:11 +0300

    Added follow mc code to the soliders

--- scenes/solider.gd
+++ scenes/solider.gd
@@ -1,1 +1,14 @@
 extends "res://scripts/boat.gd"
+var player : CharacterBody3D
+
+
+
+func _physics_process(delta):
+	if player:
+		var dir = player.global_position - global_position
+		dir.y = 0
+		var normalized_dir = dir.normalized()
+		look_at(normalized_dir)
+		velocity = normalized_dir
+		move_and_slide()
+		
