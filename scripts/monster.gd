c214bc66f6ec6327421922b1292afd3c7051f733
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-05-20 17:14:07 +0300

    removed hunting mode from monster

--- scripts/monster.gd
+++ scripts/monster.gd
@@ -1,5 +1,5 @@
 extends CharacterBody3D
-enum MODES {SEARCHING, IDLE, EATING, ATTACKING, HUNTING}
+enum MODES {SEARCHING, IDLE, EATING, HUNTING}
 
 
 var target : CharacterBody3D
@@ -12,8 +12,7 @@ var is_down = false
 
 
 var player : CharacterBody3D
-var soliders = []
-var islanders  = []
+
 
 var has_a_target = false
 
@@ -35,14 +34,10 @@ func _physics_process(delta):
 	
 	
 	match mode:
-		MODES.ATTACKING:
-			$AnimationPlayer.play("rig_001|monster_attack")
-			#$AnimationPlayer.stop()
-			return
 		MODES.IDLE:
 			return
 			
-		MODES.EATING:
+		MODES.HUNTING:
 			if player:
 				if player.global_position.distance_to(global_position) < 2:
 					if not reached_body:
@@ -59,17 +54,10 @@ func _physics_process(delta):
 
 func _on_detector_body_entered(body : CharacterBody3D):
 	
-	if eating or has_a_target:
+	if eating or has_a_target or (body.hunter != null):
 		return
 	has_a_target = true
-	if body.collision_layer == 16:
-		if not islanders.has(body):
-			islanders.push_back(body)
-		return
-	if body.collision_layer == 4:
-		if not soliders.has(body):
-			soliders.push_back(body)
-		return
+	body.hunter = self
 	
 	player = body
 	mode = MODES.HUNTING
